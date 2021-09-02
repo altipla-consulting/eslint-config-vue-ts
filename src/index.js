9a08987dc040f23bdfba9481c13a0bf8213291ad
Ernesto Alejo <ernestokarim@gmail.com>
2021-09-02 02:03:17 +0200

    Change peer to normal dependencies.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,62 @@
 
 module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
   extends: [
-    '@altipla/eslint-config-ts',
+    'eslint:recommended',
     'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  ignorePatterns: ['*.css', '/*.*'],
   rules: {
-    'vue/mustache-interpolation-spacing': ['error', 'never'],
+    'indent': 'off',
+    '@typescript-eslint/indent': ['error', 2],
 
+    'linebreak-style': ['error', 'unix'],
+
+    'quotes': 'off',
+    '@typescript-eslint/quotes': ['error', 'single', { allowTemplateLiterals: true }],
+
+    'semi': 'off',
+    '@typescript-eslint/semi': ['error', 'never'],
+
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+
+    'dot-notation': 'off',
+    '@typescript-eslint/dot-notation': 'error',
+
+    'eol-last': 'error',
+    'key-spacing': 'error',
+
+    'keyword-spacing': 'off',
+    '@typescript-eslint/keyword-spacing': ['error', { before: true, after: true }],
+
+    'no-undef': 'error',
+    'space-before-blocks': 'error',
+    'space-infix-ops': 'error',
+
+    'object-curly-spacing': 'off',
+    '@typescript-eslint/object-curly-spacing': ['error', 'always'],
+
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '_', argsIgnorePattern: '_' }],
+
+    'prefer-const': 'off',
+
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    'vue/mustache-interpolation-spacing': ['error', 'never'],
+  
     // Disable root validation until it adapts to Vue 3 where we can have multiple
     // of them. I guess this rule will disappear once that occurs.
     'vue/valid-template-root': ['off'],
@@ -14,6 +64,7 @@ module.exports = {
     'vue/no-v-for-template-key': ['off'],
   },
   plugins: [
+    '@typescript-eslint',
     'vue',
   ],
 }
