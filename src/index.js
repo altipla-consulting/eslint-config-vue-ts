1a7c71a6cdad131a0c4b80a68aab797a2a645e52
Marisa Robayo Moreno <marisarobayo@gmail.com>
2023-06-07 17:23:15 +0200

    Show error instead of warning on bad HTML indentation.

--- src/index.js
+++ src/index.js
@@ -77,6 +77,8 @@ module.exports = {
     'vue/component-definition-name-casing': ['error', 'kebab-case'],
     'vue/require-default-prop': 'off',
     'vue/singleline-html-element-content-newline': 'off',
+    'vue/no-setup-props-destructure': 'off',
+    'vue/html-indent': 'error',
 
     'no-restricted-imports': [
       'error',
